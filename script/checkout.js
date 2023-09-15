51056fe57e2b444e577eb616524e35e20eded02e
Devender Shakya <devshakya@gmail.com>
2023-09-15 16:48:28 +0530

    Cart Done

--- script/checkout.js
+++ script/checkout.js
@@ -3,18 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   addressForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    // Get form values
     const data = {
       firstName: document.getElementById("firstName").value,
       lastName: document.getElementById("lastName").value,
       phoneNumber: document.getElementById("phone").value,
-
-      address: document.getElementById("address").value,
-    //   newFirstName: document.getElementById("newfirstname").value,
-    //   newLastName: document.getElementById("newlastname").value,
-    //   newAddress: document.getElementById("newaddress").value,
-
+      address1: document.getElementById("address1").value,
+      address2: document.getElementById("address2").value,
+      city: document.getElementById("city").value,
+      state: document.getElementById("state").value,
+      postalCode: document.getElementById("postal-code").value,
+      country: document.getElementById("country").value,
     };
 
+    // Store data in local storage
     localStorage.setItem("formData", JSON.stringify(data));
     alert("Shipping address saved! Proceeding to payment.");
     window.location.href = "./payment.html";
