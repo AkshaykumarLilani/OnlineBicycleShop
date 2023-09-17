8e45493efa3238a9770f9cb47a46866667ca7ea1
Akshay Lilani <akshaylilani60@gmail.com>
2023-09-17 16:51:23 +0530

    🐛 FIX: add / in href of abou and contact

--- components/top.js
+++ components/top.js
@@ -4,10 +4,10 @@ function top(params) {
       <ul id="topbar1" class="top-navbar1">
         <a href=""><i class="fa-solid fa-location-crosshairs" style="color: #eca746;"></i><span> Your Location
             <span><b>Delhi</b></span></span></a>
-        <a href="about.html"><span>About us</span></a>
+        <a href="/about.html"><span>About us</span></a>
         <a href="#"><span>Delivery</span></a>
         <a href="#"><span>Payment</span></a>
-        <a href="contact.html"><span>Contact us</span></a>
+        <a href="/contact.html"><span>Contact us</span></a>
 
       </ul>
     </div>`    
